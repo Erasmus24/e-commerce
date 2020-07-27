0caaf86273dbe665f4029e8e9b730fa822acd274
Senzo Khoza <sphiwe@thedigitalacademy.co.za>
2020-07-27 19:34:05 +0200

    ....

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,15 @@
-
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 export default class Navbar extends Component {
     render() {
         return (
-            <div>
-                <h3>Navigation</h3>
-            </div>
+           <nav className='navbar navbar-expand-sm navbar-dark px-sm-5' >
+               <Link to='/'>
+                    <i class="fas fa-chess-rook"></i>
+               </Link>
+
+           </nav>
         )
     }
 }
