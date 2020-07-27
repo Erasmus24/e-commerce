c66092bedab01c69f3c80bb05cd8ec3d8cc0a83c
Senzo Khoza <sphiwe@thedigitalacademy.co.za>
2020-07-27 16:54:02 +0200

    ...

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
 
 function App() {
   return (
     <React.Fragment>
-      <Navbar></Navbar>
-      <h1>Worlds Greatest</h1>
+      <Navbar/>
+      <ProductList/>
+      <Details/>
+      <Cart/>
+      <Default/>
     </React.Fragment>
   );
 }
