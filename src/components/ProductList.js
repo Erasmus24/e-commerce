7cac8f9202a25de41509648ae9d519f1d9839adf
Senzo Khoza <sphiwe@thedigitalacademy.co.za>
2020-07-29 01:38:09 +0200

    ...

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
-import Product from './Product'
+import Title from './Title';
+import Product from './Product';
 
 export default class ProductList extends Component {
+    state={
+        products: []
+    }
     render() {
         return (
-            <div>
-                <Product/>
-            </div>
+            <React.Fragment>
+                <div className='py-5'> {/*py means padding bottom*/}
+                    <div className='container'>
+                        <div className='row'>
+                            <Title name='our' title='products'/>
+                        </div>
+                        
+                    </div>
+                </div>
+            </React.Fragment>
+            // <Product/>
         )
     }
 }
