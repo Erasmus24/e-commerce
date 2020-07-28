2682b1d5062cc6090ed546e0451cdf948dc6e6f3
Senzo Khoza <sphiwe@thedigitalacademy.co.za>
2020-07-29 00:29:31 +0200

    ...

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import {ButtonContainer} from './Button';
 import styled from 'styled-components';
+
 
 export default class Navbar extends Component {
     render() {
@@ -30,24 +32,5 @@ export default class Navbar extends Component {
     }
 }
 
-//styled components
 
-const ButtonContainer = styled.button`
-    text-transform: capitalize;
-    font-size: 1.4rem;
-    background: transparent;
-    border: 0.05rem solid var(--lightBlue);
-    color: var(--lightBlue);
-    border-radius: 0.5rem;
-    padding: 0.2rem 0.5rem;
-    cursor: pointer;
-    margin: 0.2rem 0.5rem 0.2rem 0;
-    transition: all 0.5s ease-in-out;
- &:hover{
-     background: var(--lightBlue);
-     color: var(--mianBlue);
- }
- &:focus{
-     outline: none;
- }
-`
+
