3891bdbe4f2219c0f32a34431f76179a1b1ebcac
Senzo Khoza <sphiwe@thedigitalacademy.co.za>
2020-07-28 12:39:55 +0200

    ....

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,18 @@ import {Link} from 'react-router-dom';
 export default class Navbar extends Component {
     render() {
         return (
-           <nav className='navbar navbar-expand-sm navbar-dark px-sm-5' >
-               <Link to='/'>
+           <nav className='navbar navbar-expand-sm bg-primary navbar-dark px-sm-5' >
+               <Link to='/' className='text-warning'>
                     <i class="fas fa-chess-rook"></i>
                </Link>
-
+               <ul className='navbar-nav align-items-center'>
+                   <li className='nav-item ml-5'>
+                       <Link to='/' className='nav-link text-warning'>
+                           Products
+                       </Link>
+                   </li>
+               </ul>
+            
            </nav>
         )
     }
