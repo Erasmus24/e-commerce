5886efa016a1a2c1bdb2595fb7323c8e0824bc4a
Senzo Khoza <sphiwe@thedigitalacademy.co.za>
2020-07-29 00:45:47 +0200

    ...

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,14 +7,14 @@ import styled from 'styled-components';
 export default class Navbar extends Component {
     render() {
         return (
-           <nav className='navbar navbar-expand-sm bg-primary navbar-dark px-sm-5' >
+           <NavWrapper className='navbar navbar-expand-sm  navbar-dark px-sm-5' >
                <Link to='/' className='text-warning'>
                     <i class="fas fa-chess-rook"></i>
                </Link>
                <ul className='navbar-nav align-items-center'>
                    <li className='nav-item ml-5'>
                        <Link to='/' className='nav-link text-warning'>
-                           Products
+                           products
                        </Link>
                    </li>
                </ul>
@@ -27,10 +27,17 @@ export default class Navbar extends Component {
                    </ButtonContainer>
                </Link>
             
-           </nav>
+           </NavWrapper>
         )
     }
 }
 
 
-
+const NavWrapper = styled.nav`
+    background: var(--mainBlue);
+    .nav-link{
+        color: var(--mainWhite) !important;
+        font-size: 1.3rem;
+        text-transform: capitalize;
+    }
+`
