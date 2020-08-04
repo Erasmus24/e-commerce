3785d9b87532d6239c3928d41354072115125ff8
Senzo Khoza <sphiwe@thedigitalacademy.co.za>
2020-08-04 10:07:44 +0200

    adding the products

---
+++ src/Data.js
@@ -0,0 +1,94 @@
+import Title from "./components/Title";
+
+export const storeProducts = [
+    {
+        id: 1,
+        title: 'Armani',
+        img: 'abdul/armani.jpg',
+        price: 330,
+        Company: 'Armani',
+        info: 'blah fjry asuiro ieyfgrnnn juuethsdg  hhjjsj',
+        inCart: 'false',
+        count:0,
+        total: 0
+        
+    },
+    {
+        id: 2,
+        title: 'Channel',
+        img: 'abdul/channel.jpg',
+        price: 330,
+        Company: 'Channel',
+        info: 'blah fjry asuiro ieyfgrnnn juuethsdg  hhjjsj',
+        inCart: 'false',
+        count:0,
+        total: 0
+        
+    },
+    {
+        id: 3,
+        title: 'Colgne12',
+        img: 'abdul/colgne12.jpg',
+        price: 330,
+        Company: 'Colonge12',
+        info: 'blah fjry asuiro ieyfgrnnn juuethsdg  hhjjsj',
+        inCart: 'false',
+        count:0,
+        total: 0
+        
+    },
+    {
+        id: 4,
+        title: 'Mercedes Benz ',
+        img: 'abdul/mercedsBenz.jpg',
+        price: 330,
+        Company: 'Mercedes Benz',
+        info: 'blah fjry asuiro ieyfgrnnn juuethsdg  hhjjsj',
+        inCart: 'false',
+        count:0,
+        total: 0
+        
+    },
+    {
+        id: 5,
+        title: 'Another1 ',
+        img: 'abdul/o.31508.jpg',
+        price: 330,
+        Company: 'Another1',
+        info: 'blah fjry asuiro ieyfgrnnn juuethsdg  hhjjsj',
+        inCart: 'false',
+        count:0,
+        total: 0
+        
+    },
+    {
+        id: 6,
+        title: 'Polo ',
+        img: 'abdul/polo.jpg',
+        price: 330,
+        Company: 'Polo',
+        info: 'blah fjry asuiro ieyfgrnnn juuethsdg  hhjjsj',
+        inCart: 'false',
+        count:0,
+        total: 0
+        
+    },
+    {
+        id: 7,
+        title: 'Gucci',
+        img: 'abdul/gucci.jpg',
+        price: 330,
+        Company: 'Gucci',
+        info: 'blah fjry asuiro ieyfgrnnn juuethsdg  hhjjsj',
+        inCart: 'false',
+        count:0,
+        total: 0
+        
+    },
+];                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+
+
+
+
+
+   
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import Title from './Title';
 import Product from './Product';
+import {storeProducts} from '../Data';
 
 export default class ProductList extends Component {
     state={
-        products: []
+        products: storeProducts
     }
     render() {
+        console.log(this.state.products);
         return (
             <React.Fragment>
                 <div className='py-5'> {/*py means padding bottom*/}
