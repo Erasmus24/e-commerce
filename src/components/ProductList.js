3b6fc386690503fcf4c054c7dea58936d97b7f98
Senzo Khoza <sphiwe@thedigitalacademy.co.za>
2020-08-04 17:22:35 +0200

    ....

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -18,7 +18,7 @@ export default class ProductList extends Component {
                         <ProductConsumer>
                             { value => {
                                 return value.products.map( product =>{
-                                    return <Product product ={product} />;
+                                    return <Product key={product.id} product ={product} />;
                                 });
                             }}
                         </ProductConsumer>
