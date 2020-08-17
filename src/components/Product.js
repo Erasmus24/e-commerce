9cc5c4bffc4ca22f5347da47d0c44d2a5fe983dc
Senzo Khoza <sphiwe@thedigitalacademy.co.za>
2020-08-17 11:07:51 +0200

    ...

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,7 +5,7 @@ import { ProductConsumer } from "../context";
 
 export default class Product extends Component {
   render() {
-    const { id, Title, img, price, inCart } = this.props.product;
+    const { id, title, img, price, inCart } = this.props.product;
     return (
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
         <div className="card">
@@ -20,20 +20,28 @@ export default class Product extends Component {
             </Link>
             <button
               className="card-btn"
-              disabled={inCart ? true : false}
+              disabled={inCart ? true : false} //CONDITIONAL RENDERING!!!!
               onClick={() => {
                 console.log("now added to the cart");
               }}
             >
-            {inCart ? (
-              <p className="text-capitalize mb-0" disabled>
-                {""}
-                In Cart
-              </p>
-            ) : (
-              <i className="fas fa-cart-plus" />
-            )}
+              {inCart ? ( //CONDITIONAL RENDERING!!!!
+                <p className="text-capitalize mb-0" disabled>
+                  {""}
+                  In Cart
+                </p>
+              ) : (
+                <i className="fas fa-cart-plus" />
+              )}
             </button>
+          </div>
+          {/* Card Footer */}
+          <div className="card-footer d-flex justify-content-between">
+            <p className="align-self-center mb-0">{title}</p>
+            <h5 className="text-blue font-italic mb-0">
+              <span className="mr-1">R</span>
+              {price}
+            </h5>
           </div>
         </div>
       </ProductWrapper>
