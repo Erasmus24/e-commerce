36e8b4d1d4a6fa68bb80b9c27ec4b70426a33241
Senzo Khoza <sphiwe@thedigitalacademy.co.za>
2020-08-19 00:33:34 +0200

    ...

--- src/components/Product.js
+++ src/components/Product.js
@@ -10,32 +10,34 @@ export default class Product extends Component {
     return (
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
         <div className="card">
-          <div
-            className="img-container pd-5"
-            onClick={() =>
-              console.log("card in img-container has been clicked")
-            }
-          >
-            <Link to="/details">
-              <img src={img} alt="product" className="card-img-top" />
-            </Link>
-            <button
-              className="card-btn"
-              disabled={inCart ? true : false} //CONDITIONAL RENDERING!!!!
-              onClick={() => {
-                console.log("now added to the cart");
-              }}
-            >
-              {inCart ? ( //CONDITIONAL RENDERING!!!!
-                <p className="text-capitalize mb-0" disabled>
-                  {""}
-                  In Cart
-                </p>
-              ) : (
-                <i className="fas fa-cart-plus" />
-              )}
-            </button>
-          </div>
+          <ProductConsumer>
+            {(value) => (
+              <div
+                className="img-container pd-5"
+                onClick={() => value.handleDetail(id)}
+              >
+                <Link to="/details">
+                  <img src={img} alt="product" className="card-img-top" />
+                </Link>
+                <button
+                  className="card-btn"
+                  disabled={inCart ? true : false} //CONDITIONAL RENDERING!!!!
+                  onClick={() => {
+                    value.addToCart(id);
+                  }}
+                >
+                  {inCart ? ( //CONDITIONAL RENDERING!!!!
+                    <p className="text-capitalize mb-0" disabled>
+                      {""}
+                      In Cart
+                    </p>
+                  ) : (
+                    <i className="fas fa-cart-plus" />
+                  )}
+                </button>
+              </div>
+            )}
+          </ProductConsumer>
           {/* Card Footer */}
           <div className="card-footer d-flex justify-content-between">
             <p className="align-self-center mb-0">{title}</p>
