2ef609815cb041cbe967839fc4d7e50550229f8a
Senzo Khoza <sphiwe@thedigitalacademy.co.za>
2020-08-19 02:54:18 +0200

    ...

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -20,9 +20,22 @@ export default class Modal extends Component {
                   <div className="row">
                     <div
                       id="modal"
-                      className="col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize"
+                      className="col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize p-5"
                     >
                       <h5>item added to the cart</h5>
+                      <img src={img} className="img-fluid" alt="product" />
+                      <h4>{title}</h4>
+                      <h5 className="text-muted">price: R{price}</h5>
+                      <Link to="/">
+                        <ButtonContainer onClick={() => closeModal()}>
+                          continue shopping
+                        </ButtonContainer>
+                      </Link>
+                      <Link to="/cart">
+                        <ButtonContainer onClick={() => closeModal()}>
+                          go to cart
+                        </ButtonContainer>
+                      </Link>
                     </div>
                   </div>
                 </div>
