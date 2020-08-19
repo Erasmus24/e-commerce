52a912804e0b1d700bbe4deb46e009cda7d14ade
Senzo Khoza <sphiwe@thedigitalacademy.co.za>
2020-08-19 02:07:03 +0200

    ...

---
+++ src/components/Modal.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import { ProductConsumer } from "../context";
+import { ButtonContainer } from "./Button";
+import { Link } from "react-router-dom";
+
+export default class Modal extends Component {
+  render() {
+    return (
+      <ProductConsumer>
+        {(value) => {
+          const { modalOpen, closeModal } = value;
+          const { img, title, price } = value.modalProduct;
+          if (!modalOpen) {
+            return null;
+          } else {
+            return (
+              <ModalContainer>
+                <div className="contaner">
+                  <div className="row">
+                    <div className="col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize">
+                      <h5>item added to the cart</h5>
+                    </div>
+                  </div>
+                </div>
+              </ModalContainer>
+            );
+          }
+        }}
+      </ProductConsumer>
+    );
+  }
+}
+
+const ModalContainer = styled.div``;
