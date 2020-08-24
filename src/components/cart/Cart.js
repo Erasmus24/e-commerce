84bb0a30755fb34b1faa25d77476fcadcee27bec
Senzo Khoza <sphiwe@thedigitalacademy.co.za>
2020-08-24 23:25:00 +0200

    ..

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import Title from '../Title'
+
 
 export default class Cart extends Component {
   render() {
     return (
-      <div>
-        <h3>This is the Cart</h3>
-      </div>
+      <section>
+        <Title name='your' title='cart'/>
+      </section>
     );
   }
 }
