1ab372a73bb0152c08fd8cab9670947a4491a42a
Senzo Khoza <sphiwe@thedigitalacademy.co.za>
2020-09-03 03:29:14 +0200

    ...

--- src/components/Product.js
+++ src/components/Product.js
@@ -108,6 +108,7 @@ const ProductWrapper = styled.div`
     border-radius: 0.5rem 0 0 0;
     transform: translate(100%, 100%);
     transition: all 1s linear;
+    outline:none;
   }
   .img-container:hover .card-btn {
     transform: translate(0, 0);
